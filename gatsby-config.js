31ba1ef250ed87bb06595a4c20647ed21038d453
Gaël GERARD <git@gaelgerard.com>
2017-11-21 09:35:02 +0100

    test avec wp.com site

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,17 +16,17 @@ module.exports = {
         * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
         * Example : 'gatsbyjswpexample.wordpress.com' or 'www.example-site.com'
         */
-        baseUrl: `dev.gaelgerard.com`,
+        baseUrl: `gaelgerard.wordpress.com`,
         // The protocol. This can be http or https.
-        protocol: `http`,
+        protocol: `https`,
         // Indicates whether the site is hosted on wordpress.com.
         // If false, then the asumption is made that the site is self hosted.
         // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
         // If your site is hosted on wordpress.org, then set this to false.
-        hostingWPCOM: false,
+        hostingWPCOM: true,
         // If useACF is true, then the source plugin will try to import the Wordpress ACF Plugin contents.
         // This feature is untested for sites hosted on Wordpress.com
-        useACF: true,
+        useACF: false,
       },
     },
     `gatsby-transformer-sharp`,
